57e8f1eb383b1fbea45b509a9c90042b306aa37d
gh-orion <ashwinpate434143@gmail.com>
2021-11-09 20:10:36 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-          <Route exact path="/">
+          <Route exact path="/Personal-portfolio">
           <Home />
           </Route>
           <Route exact path="/about">
